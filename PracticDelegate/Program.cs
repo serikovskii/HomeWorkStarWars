db0a50cda564565ae41bc66c869e1680db842e0b
Azat <46015824+serikovskii@users.noreply.github.com>
2019-03-19 19:29:40 +0600

    dz

---
+++ PracticDelegate/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace PracticDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            bool findForXml = true;
+            List<StarWarsPeople> peoples = new List<StarWarsPeople>();
+            var serializerXml = new XmlSerializer(typeof(List<StarWarsPeople>));
+            Console.WriteLine("Введите номер персонажа: ");
+            choice = Console.ReadLine();
+
+            StarWarsPeople result = new StarWarsPeople();
+            if (File.Exists("xmlPerson.xml"))
+            {
+                using (FileStream stream = new FileStream("xmlPerson.xml", FileMode.Open))
+                {
+                    peoples = (List<StarWarsPeople>)serializerXml.Deserialize(stream);
+                }
+                foreach (StarWarsPeople sw in peoples)
+                {
+                    if (sw.Id == Int32.Parse(choice))
+                    {
+                        Console.WriteLine($"Персонаж с xml \nName: {result.Name}\n Birthd: {result.BirthYear}\n Eye: {result.EyeColor}\n Gender: {result.Gender}" +
+                    $"\n Hair: {result.HairColor}\n Height: {result.Height} \n Homeworld: {result.Homeworld}\n Skin: {result.SkinColor}");
+                        findForXml = false;
+                        break;
+                    }
+                    
+                }
+            }
+            
+            if (findForXml)
+            {
+                WebClient client = new WebClient();
+                string json = client.DownloadString("https://swapi.co/api/people/" + choice + "/");
+                result = JsonConvert.DeserializeObject<StarWarsPeople>(json);
+                result.Id = Int32.Parse(choice);
+                Console.WriteLine($"Персонаж с сайта \nName: {result.Name}\n Birthd: {result.BirthYear}\n Eye: {result.EyeColor}\n Gender: {result.Gender}" +
+                    $"\n Hair: {result.HairColor}\n Height: {result.Height} \n Homeworld: {result.Homeworld}\n Skin: {result.SkinColor}");
+                peoples.Add(result);
+                
+                using (FileStream stream = new FileStream("xmlPerson.xml", FileMode.OpenOrCreate))
+                {
+                    serializerXml.Serialize(stream, peoples);
+                }
+            }
+            
+        }
+
+    }
+}
---
+++ PracticDelegate/StarWarsPeople.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticDelegate
+{
+    public class StarWarsPeople
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Height { get; set; }
+        public string Mass { get; set; }
+        [JsonProperty(PropertyName = "hair_color")]
+        public string HairColor { get; set; }
+        [JsonProperty(PropertyName = "skin_color")]
+        public string SkinColor { get; set; }
+        [JsonProperty(PropertyName = "eye_color")]
+        public string EyeColor { get; set; }
+        [JsonProperty(PropertyName = "birth_year")]
+        public string BirthYear { get; set; }
+        public string Gender { get; set; }
+        public string Homeworld { get; set; }
+        public string[] Films { get; set; }
+        public string[] Species { get; set; }
+        public string[] Vehicles { get; set; }
+        public string[] Starships { get; set; }
+        public string Created { get; set; }
+        public string Edited { get; set; }
+        public string Url { get; set; }
+        
+    }
+}
